2676d72657a84d5dbacd47a20510637ad3a7b3a1
KasperNooteboom <kasper.nooteboom@dimaros.nl>
2022-12-04 22:18:46 +0100

    Add Day 4

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2022;
 
-var solver = new _3();
+var solver = new _4();
 solver.Run();
---
+++ AdventOfCode2022/_4.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode2022;
+public class _4 : Base
+{
+    protected override void Action()
+    {
+        //UseExample();
+        List<Tup<Ran, Ran>> pairs = new();
+        foreach (string line in InputLines)
+        {
+            string[] elves = line.Split(',');
+            Ran e1 = new Ran(elves[0]);
+            Ran e2 = new Ran(elves[1]);
+            pairs.Add(new(e1, e2));
+        }
+
+        int contained = 0;
+        foreach (var pair in pairs)
+        {
+            if ((pair.a.start <= pair.b.start && pair.a.end >= pair.b.end) ||
+                (pair.b.start <= pair.a.start && pair.b.end >= pair.a.end))
+            {
+                contained++;
+            }
+        }
+        WriteLine(contained);
+
+        B();
+
+        int overlapping = 0;
+        foreach (var pair in pairs)
+        {
+            if ((pair.a.start <= pair.b.end && pair.a.end >= pair.b.start) ||
+                (pair.b.start <= pair.a.end && pair.b.end >= pair.a.start))
+            {
+                overlapping++;
+            }
+        }
+        WriteLine(overlapping);
+    }
+}
+
+public class Ran
+{
+    public int start;
+    public int count;
+    public int end;
+
+    public Ran(string dashed)
+    {
+        string[] split = dashed.Split('-');
+        int start = int.Parse(split[0]);
+        int end = int.Parse(split[1]);
+        this.start = start;
+        this.count = end - start + 1;
+        this.end = end;
+    }
+}
