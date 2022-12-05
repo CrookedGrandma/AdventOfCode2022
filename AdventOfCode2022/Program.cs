380f1e408a1fdb8528ef3f2e77fc5eab2605881f
CrookedGrandma <k.t.noot@live.nl>
2022-12-05 13:28:31 +0100

    Use reflection to find the latest puzzle class

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,12 @@
 ﻿using AdventOfCode2022;
+using System.Reflection;
 
-var solver = new _5();
-solver.Run();
+var type = Assembly.GetExecutingAssembly()
+    .DefinedTypes
+    .Where(t => t.BaseType!.Name == "Base")
+    .Where(t => t.Name != "_Test")
+    .OrderBy(t => int.Parse(t.Name[1..]))
+    .Last();
+
+var solver = (Base?)Activator.CreateInstance(type);
+solver?.Run();
