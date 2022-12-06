971415a8551a861c021c651ecb3cd227d4c40e79
KasperNooteboom <kasper.nooteboom@dimaros.nl>
2022-12-06 09:39:20 +0100

    Make _Test class into a template

--- AdventOfCode2022/_Test.cs
+++ AdventOfCode2022/_Test.cs
@@ -3,7 +3,13 @@ public class _Test : Base
 {
     protected override void Action()
     {
-        OutputWriter.WriteLine("test");
-        OutputWriter.WriteLine(InputLines[0]);
+        foreach (string line in InputLines)
+        {
+
+        }
+
+        B();
+
+
     }
 }
