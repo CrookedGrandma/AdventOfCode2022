ba0fb8dde8606f3e1d4ff94c488f1bfe4eb8f585
KasperNooteboom <kasper.nooteboom@dimaros.nl>
2022-12-06 10:26:42 +0100

    Add Day 6

---
+++ AdventOfCode2022/_6.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2022;
+public class _6 : Base
+{
+    protected override void Action()
+    {
+        //UseExample();
+        string input = InputLines[0];
+
+        int buffPos = -1;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (i < 4) continue;
+            char[] last4 = input[(i - 4)..i].ToCharArray();
+            if (last4.Distinct().Count() == 4)
+            {
+                buffPos = i;
+                break;
+            }
+        }
+        WriteLine(buffPos);
+
+        B();
+
+        buffPos = -1;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (i < 14) continue;
+            char[] last4 = input[(i - 14)..i].ToCharArray();
+            if (last4.Distinct().Count() == 14)
+            {
+                buffPos = i;
+                break;
+            }
+        }
+        WriteLine(buffPos);
+    }
+}
