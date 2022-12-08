fadec15ec3b52646378797b0a260cc207eade5d2
KasperNooteboom <kasper.nooteboom@dimaros.nl>
2022-12-08 15:29:27 +0100

    Add extra extension methods for 2D arrays

--- AdventOfCode2022/Base.cs
+++ AdventOfCode2022/Base.cs
@@ -64,6 +64,12 @@ public class Tup<T1, T2>
 
 public static class Extensions
 {
+    public static T[] GetRow<T>(this T[,] matrix, int y)
+        => matrix.GetRangeX(y, 0, matrix.GetLength(0));
+
+    public static T[] GetCol<T>(this T[,] matrix, int x)
+        => matrix.GetRangeY(x, 0, matrix.GetLength(1));
+
     public static T[] GetRangeX<T>(this T[,] matrix, int y, int xstart, int xcount)
         => Enumerable.Range(xstart, xcount).Select(x => matrix[x, y]).ToArray();
 
