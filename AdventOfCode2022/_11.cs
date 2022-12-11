9126b884f5c77bd37a71e9f4699615e3d46ac585
CrookedGrandma <k.t.noot@live.nl>
2022-12-11 23:55:03 +0100

    Add incomplete Day 11 :(

---
+++ AdventOfCode2022/_11.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022;
+public class _11 : Base {
+    protected override void Action() {
+        //UseExample();
+        List<Monkey> monkeys = new();
+        for (int i = 0; i < InputLines.Count; i += 7) {
+            List<ulong> items = Regex.Split(InputLines[i + 1][18..], @", ").Select(s => ulong.Parse(s)).ToList();
+            string operation = InputLines[i + 2][19..];
+            ulong div = ulong.Parse(InputLines[i + 3][21..]);
+            int mT = int.Parse(InputLines[i + 4][29..]);
+            int mF = int.Parse(InputLines[i + 5][30..]);
+            monkeys.Add(new(items, operation, div, mT, mF));
+        }
+
+        for (int r = 0; r < 1000; r++) {
+            foreach (Monkey m in monkeys) {
+                m.ThrowItems(monkeys, r);
+            }
+        }
+
+        var inspections = monkeys.Select(m => m.inspections).ToList();
+        var sorted = monkeys.OrderByDescending(x => x.inspections).ToList();
+        //int answer = sorted.Take(2).Select(x => x.inspections).Aggregate((a, b) => a * b);
+        long answer = (long)sorted[0].inspections * (long)sorted[1].inspections;
+        WriteLine(answer);
+
+        B();
+
+
+    }
+}
+
+public class Monkey {
+    public int inspections = 0;
+    public Queue<ulong> items = new();
+    public Func<ulong, ulong> operation;
+
+    private ulong divisibility;
+    private int monkeTrue, monkeFalse;
+    
+    public Monkey(IEnumerable<ulong> items, string operation, ulong div, int mT, int mF) {
+        foreach (ulong item in items)
+            this.items.Enqueue(item);
+
+        if (operation == "old * old")
+            this.operation = x => x * x;
+        else {
+            char op = operation[4];
+            ulong v = ulong.Parse(operation[6..]);
+            this.operation = op switch {
+                '+' => (x => x + v),
+                '*' => (x => x * v),
+                _ => throw new Exception("seek therapy")
+            };
+        }
+
+        this.divisibility = div;
+        this.monkeTrue = mT;
+        this.monkeFalse = mF;
+    }
+
+    public void ThrowItems(List<Monkey> monkeys, int round) {
+        while (items.Count > 0) {
+            ThrowFirstItem2(monkeys, round);
+        }
+    }
+
+    private void ThrowFirstItem(List<Monkey> monkeys) {
+        ulong item = items.Dequeue();
+        ulong worry = operation(item) / 3;
+        this.inspections++;
+        if (worry % divisibility == 0)
+            monkeys[monkeTrue].items.Enqueue(worry);
+        else
+            monkeys[monkeFalse].items.Enqueue(worry);
+    }
+
+    private void ThrowFirstItem2(List<Monkey> monkeys, int round) {
+        ulong item = items.Dequeue();
+        ulong worry = operation(item) % 9699690;
+        this.inspections++;
+        if (worry % divisibility == 0)
+            monkeys[monkeTrue].items.Enqueue(worry);
+        else
+            monkeys[monkeFalse].items.Enqueue(worry);
+    }
+}
